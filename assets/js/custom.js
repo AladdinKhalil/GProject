cde5ea4e07d03ed25ed65dc2ebaf9bd652366f47
Aladdin <alaaalaa437@gmail.com>
2023-06-15 09:38:18 +0300

    animations

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,101 +15,25 @@
 	});
 	
 	$(document).ready(function() {
-		// Add click event to filter buttons
-		$('.filters li').click(function() {
-		  // Remove active class from all buttons
+		// Add click event listener to filter buttons
+		$('.filters li').on('click', function() {
+		  // Remove active class from all filter buttons
 		  $('.filters li').removeClass('active');
-		  // Add active class to clicked button
+		  // Add active class to clicked filter button
 		  $(this).addClass('active');
 		  // Get filter value
-		  var filterValue = $(this).attr('data-filter');
-		  // Show/hide cards based on filter value
-		  if (filterValue == '*') {
-			$('.card').show();
-		  } else {
-			$('.card').hide();
-			$(filterValue).show();
-		  }
+		  var filterValue = $(this).data('filter');
+		  // Hide all cards
+		  $('.card').hide();
+		  // Show filtered cards with animation
+		  $(filterValue).fadeIn(1000);
 		});
 	  });
-	  
 
 
-	const Accordion = {
-	  settings: {
-	    // Expand the first item by default
-	    first_expanded: false,
-	    // Allow items to be toggled independently
-	    toggle: false
-	  },
+	
 
-	  openAccordion: function(toggle, content) {
-	    if (content.children.length) {
-	      toggle.classList.add("is-open");
-	      let final_height = Math.floor(content.children[0].offsetHeight);
-	      content.style.height = final_height + "px";
-	    }
-	  },
-
-	  closeAccordion: function(toggle, content) {
-	    toggle.classList.remove("is-open");
-	    content.style.height = 0;
-	  },
-
-	  init: function(el) {
-	    const _this = this;
-
-	    // Override default settings with classes
-	    let is_first_expanded = _this.settings.first_expanded;
-	    if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
-	    let is_toggle = _this.settings.toggle;
-	    if (el.classList.contains("is-toggle")) is_toggle = true;
-
-	    // Loop through the accordion's sections and set up the click behavior
-	    const sections = el.getElementsByClassName("accordion");
-	    const all_toggles = el.getElementsByClassName("accordion-head");
-	    const all_contents = el.getElementsByClassName("accordion-body");
-	    for (let i = 0; i < sections.length; i++) {
-	      const section = sections[i];
-	      const toggle = all_toggles[i];
-	      const content = all_contents[i];
-
-	      // Click behavior
-	      toggle.addEventListener("click", function(e) {
-	        if (!is_toggle) {
-	          // Hide all content areas first
-	          for (let a = 0; a < all_contents.length; a++) {
-	            _this.closeAccordion(all_toggles[a], all_contents[a]);
-	          }
-
-	          // Expand the clicked item
-	          _this.openAccordion(toggle, content);
-	        } else {
-	          // Toggle the clicked item
-	          if (toggle.classList.contains("is-open")) {
-	            _this.closeAccordion(toggle, content);
-	          } else {
-	            _this.openAccordion(toggle, content);
-	          }
-	        }
-	      });
-
-	      // Expand the first item
-	      if (i === 0 && is_first_expanded) {
-	        _this.openAccordion(toggle, content);
-	      }
-	    }
-	  }
-	};
-
-	(function() {
-	  // Initiate all instances on the page
-	  const accordions = document.getElementsByClassName("accordions");
-	  for (let i = 0; i < accordions.length; i++) {
-	    Accordion.init(accordions[i]);
-	  }
-	})();
-
+	
 
 	$('.owl-service-item').owlCarousel({
 		items:3,
@@ -131,25 +55,7 @@
 		  }
 	  })
 
-	$('.owl-courses-item').owlCarousel({
-		items:4,
-		loop:true,
-		dots: true,
-		nav: true,
-		autoplay: true,
-		margin:30,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:2
-			  },
-			  1000:{
-				  items:4
-			  }
-		  }
-	  })
+	
 	
 
 	// Menu Dropdown Toggle
